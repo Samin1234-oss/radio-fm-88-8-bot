5f146845aeb8992f54f425adb183e30f003bc5c0
Samin1234-oss <74984726+Samin1234-oss@users.noreply.github.com>
2026-03-09 18:29:40 +0600

    Fix start.sh: use curl to download ffmpeg

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# Download small audio-only ffmpeg binary at start
-wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-i686-static.tar.xz -O ffmpeg.tar.xz
-tar -xf ffmpeg.tar.xz
-mv ffmpeg*/ffmpeg ./ffmpeg
-chmod +x ./ffmpeg
+# Download ffmpeg if it doesn't exist
+if [ ! -f "./ffmpeg" ]; then
+    echo "📥 Downloading ffmpeg..."
+    curl -LO https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz
+    tar -xf ffmpeg-release-i686-static.tar.xz
+    mv ffmpeg-*-static/ffmpeg ./ffmpeg
+    chmod +x ./ffmpeg
+    rm -rf ffmpeg-release-i686-static.tar.xz ffmpeg-*-static
+fi
 
-# Run bot
-python radio_fm_88_8_bot.py
+# Run the bot
+python3 radio_fm_88_8_bot.py
